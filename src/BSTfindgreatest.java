e42d7ed4189496367d00e17fe6fe620ed1ea7418
Ho Jeannie <hc114@duke.edu>
2015-11-07 14:27:24 -0800

    Bulls and Cows 90% done, some test cases not working

--- src/BSTfindgreatest.java
+++ src/BSTfindgreatest.java
@@ -1,10 +1,50 @@
-   function findLargestSmallerKey(root, x):
-      result = null
-      while (root != null):
-         if (root.key < x):
-            result = root.key
-            root = root.right
-         else
-            root = root.left
-      return resul
-   
+
+public class BSTfindgreatest {
+		
+		public Node(int key){
+			int Key=key;
+			Node leftchild;
+			Node rightchild;
+		}
+		
+		public Boolean hasChildren(){
+			if(leftchild==null && rightchild == null){
+				return false;
+			}else{
+				return true;
+			}
+		}
+		
+		
+		
+	
+	
+
+	static void createMap(Node root, int x){
+		(root.hasChildren()==false && root.Key>=x){
+			return 0;
+		}elseif(root.hasChildren()==false && root.Key<x){
+			return root;
+		}
+
+		(root.leftchild.Key>=x){
+			root=root.leftchild;
+			createMap(root,x);
+		}else{
+			root=root.leftchild;
+			if(root.rightchild!=null){
+				root=root.rightchild;
+						createMap(root, x);
+			}         
+		}
+
+	}
+	public static void main(String[] args) {
+		Node example=new Node(9);
+		
+	}
+
+}
+
+
+
--- src/BullsandCows.java
+++ src/BullsandCows.java
@@ -1,5 +1,36 @@
+import java.util.HashMap;
+
 public class BullsandCows {
-	public static void main(){
-		
+	static String getHint(String secret, String guess) {
+		HashMap<Character, Integer> secretMap = new HashMap<>();
+		// put the secret into a map
+		for (int i = 0; i < secret.length(); i++) {
+			secretMap.put(secret.charAt(i), i);
+		}
+
+		// Count the number of bulls and cows
+		int bulls = 0;
+		int cows = 0;
+
+		for (int j = 0; j < guess.length(); j++) {
+			if(!secretMap.containsKey(guess.charAt(j))){
+				continue;
+			}
+			if (j == secretMap.get(guess.charAt(j))) {
+				bulls++;
+			} else {
+				cows++;
+			}
+		}
+		System.out.println(secretMap.toString());
+		String statement=Integer.valueOf(bulls)+"B"+Integer.valueOf(cows)+"C";
+		return statement;
+	}
+
+	public static void main(String[] args) {
+		String secret = "11";
+		String guess= "10";
+		String result = getHint(secret,guess);
+		System.out.println(result);
 	}
 }
