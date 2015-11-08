8fc353dd1e549fce7921f598e3b52b11bad9a218
Ho Jeannie <hc114@duke.edu>
2015-11-07 20:52:20 -0800

    rearranged the map issue, now need to figure out how to deal with duplicate keys

--- src/BullsandCows.java
+++ src/BullsandCows.java
@@ -1,35 +1,47 @@
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class BullsandCows {
 	static String getHint(String secret, String guess) {
 		HashMap<Character, Integer> secretMap = new HashMap<>();
-		// put the secret into a map
-		for (int i = 0; i < secret.length(); i++) {
-			secretMap.put(secret.charAt(i), i);
-		}
-
-		// Count the number of bulls and cows
+		HashMap<Character,Integer> guessMap=new HashMap<>();
+		// put the secret/guess into a integer/index  map
+		// Count the number of bulls
 		int bulls = 0;
 		int cows = 0;
 
-		for (int j = 0; j < guess.length(); j++) {
-			if(secretMap.containsKey(guess.charAt(j))){
-				if (j == secretMap.get(guess.charAt(j))) {
-					secretMap.remove((guess.charAt(j)));
+		for (int i = 0; i < secret.length(); i++) {
+			secretMap.put(secret.charAt(i), i);
+			guessMap.put(guess.charAt(i),i);
+		}
+		
+		Iterator sIterator=secretMap.entrySet().iterator();
+		Iterator gIterator=guessMap.entrySet().iterator();
+		while(sIterator.hasNext()){
+			Map.Entry<Character, Integer> sPair = (Map.Entry)sIterator.next();
+			Map.Entry<Character, Integer> gPair = (Map.Entry)gIterator.next();
+			if(sPair.getKey()==gPair.getKey()){
+				if(sPair.getValue()==gPair.getValue()){
 					bulls++;
 				}else{
 					cows++;
 				}
-			}	
+				secretMap.remove(sPair);
+				guessMap.remove(gPair);
+			}
+			System.out.println(secretMap.toString());
+			System.out.println(guessMap.toString());
 		}
+
 		System.out.println(secretMap.toString());
-		String statement=Integer.valueOf(bulls)+"B"+Integer.valueOf(cows)+"C";
+		String statement=Integer.valueOf(bulls)+"A"+Integer.valueOf(cows)+"B";
 		return statement;
 	}
 
 	public static void main(String[] args) {
-		String secret = "11";
-		String guess= "10";
+		String secret = "1234";
+		String guess= "0111";
 		String result = getHint(secret,guess);
 		System.out.println(result);
 	}
